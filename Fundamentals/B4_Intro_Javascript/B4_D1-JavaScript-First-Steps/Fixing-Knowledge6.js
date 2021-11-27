9f7c8291ddb63075c41a47a1782ed84e9c8b396e
Anderson Rodrigues da Silva <omicron_rpl@hotmail.com>
2021-11-27 14:05:44 -0300

    Fixing Knowledge 6 done

---
+++ Fundamentals/B4_Intro_Javascript/B4_D1-JavaScript-First-Steps/Fixing-Knowledge6.js
@@ -0,0 +1,16 @@
+var candidateState;
+var possibilities = ['Aprovada', 'Lista', 'Reprovada']
+candidateState = possibilities[Math.floor(Math.random() * possibilities.length)];
+console.log(candidateState)
+
+switch (candidateState) {
+    case 'Aprovada':
+        console.log('Parabéns, você passou no processo seletivo')
+        break;
+    case 'Lista':
+        console.log('Não se desanime, você está na nossa lista de espera')
+        break;
+    case 'Reprovada':
+        console.log('Infelizmente, você não passou no nosso processo selectivo.')
+        break;
+}
