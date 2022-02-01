ba8a1eddc410cd2f4f575acfbb975ac596b27620
Anderson Rodrigues da Silva <omicron_rpl@hotmail.com>
2022-02-01 13:15:47 -0300

    Exercice Done

--- Fundamentals/B9_Assync_and_Callbacks/B9_D2/Exercice_D2-2.js
+++ Fundamentals/B9_Assync_and_Callbacks/B9_D2/Exercice_D2-2.js
@@ -1,19 +1,27 @@
-const cryptoContainer = document.getElementById('cryptos');
+const cryptoContainer = document.getElementById("cryptos");
+
+const addShit = (arr) => {
+  arr.map(({ name, symbol, priceUsd, explorer }, index) => {
+    const dot = priceUsd.indexOf(".");
+    let crypto = document.createElement("a");
+    crypto.innerText = `${name} (${symbol}): ${priceUsd.slice(0, dot + 3)} USD`;
+    crypto.className = "coin";
+    crypto.href = explorer;
+    crypto.target = '_blank'
+    if (index < 10) cryptoContainer.appendChild(crypto);
+  });
+};
 
 const something = async () => {
-    const request = {
-        'method': 'GET',        
-        'Content-Type': 'application/json; charset=utf-8',
-        'Connection': 'keep-alive',                               
-      };
-    const promise = await fetch('https://api.coincap.io/v2/assets', request);
-    const results = await promise.json(); 
-    results.data.map(({ name, symbol, priceUsd }, index) => {
-        let crypto = document.createElement('p');
-        crypto.innerText = `${name} (${symbol}): ${priceUsd}`;
-        crypto.className = 'coin';
-        if (index <= 10) cryptoContainer.appendChild(crypto);               
-    });       
-}
+  const request = {
+    method: "GET",
+    "Content-Type": "application/json; charset=utf-8",
+    Connection: "keep-alive",
+  };
+  const promise = await fetch("https://api.coincap.io/v2/assets", request);
+  const results = await promise.json();
+  console.log(results);
+  addShit(results.data);
+};
 
 window.onload = something();
